a36ee3971fa49a6c38ea8d376204855505d4054f
Jonathan <mcjhona069@gmail.com>
2020-09-14 19:24:41 -0500

    Intersection Observer

---
+++ js/02-app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const observer = new IntersectionObserver( entries =>{
+        if(entries[0].isIntersecting){
+            console.log('Ya está visible');
+        }
+    });
+
+    observer.observe(document.querySelector('.premium'));
+
+
+});
